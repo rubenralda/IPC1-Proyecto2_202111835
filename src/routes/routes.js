1e7dbf68ed20aa0db6f864d97418cb39e1006922
rubenralda <rubenralda@gmail.com>
2022-04-22 14:01:55 -0600

    Inicio secion backend

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,22 +1,35 @@
 const { Router } = require('express');
 const router = Router();
 
-
-//obtener usuarios
-router.get('/usuarios',(req,res)=>{
+//inicio sesion
+router.post('/',(req,res)=>{
+    let usu = require('./usuarios.json');
+    console.log(req.body);
+    var nombre = req.body.nombre;
+    var contra= req.body.contra;
     var respuesta = {
-        Mensaje:"Hola mundo"
+        Mensaje:false
+    }
+    for (let index = 0; index < usu.length; index++) {
+        if (usu[index].Usuario == nombre && usu[index].Password==contra){
+            respuesta = {
+                Mensaje:true
+            }
+        } 
     }
     res.send(respuesta);
 });
+
 //obtener pokemons
 router.get('/pokedex',(req,res)=>{
-    res.send('Hola mundo desde mi primer Backend con NodeJS');
+    let pokedex= require('./pokedex.json');
+    res.send(pokedex);
 });
 
 //obtener por numero
 router.post('/obnumero',(req,res)=>{
     let pokedex = require('./pokedex.json');
+    
     res.send(pokedex);
 });
 
